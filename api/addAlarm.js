04c2d721ceddadddca78cc0f4fa87572a501dd7a
Dipp3r <aruneesh4@gmail.com>
2024-03-11 10:21:08 +0530

    fixing

--- api/addAlarm.js
+++ api/addAlarm.js
@@ -1,11 +1,12 @@
 const {ConnectDB} = require("../db-connection.js");
+const { faker } = require("@faker-js/faker");
 
 const addAlarm = async function(req,res){
     const db = await ConnectDB();
     let {attributes, collectionName} = await req.body;
     attributes = {
-        createdAt: Date.now(),
-        updatedAt: Date.now(),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.past(),
         ...attributes
     }
     try {
